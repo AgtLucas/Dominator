23aa40df2584308afc72de99bb8ec0c7063c4d77
Lucas <lucas@agtlucas.com>
2014-11-29 11:18:08 -0200

    Update components

---
+++ components/Html.jsx
@@ -0,0 +1,24 @@
+// Require React
+var React = require('react');
+
+var Component = React.createClass({
+  render: function () {
+    return (
+      <html>
+        <head>
+          <meta charSet="utf-8" />
+          <title>{this.props.title}</title>
+          <meta name="viewport" content="width=device-width, user-scalable=no" />
+          <link rel="stylesheet" href="/public/main.css">
+        </head>
+        <body>
+          <section id="todoapp" dangerouslySetInnerHTML={{__html: this.props.markup}}></section>
+          <script dangerouslySetInnerHTML={{__html: this.props.state}}></script>
+          <script src="/public/js/client.js"></script>
+        </body>
+      </html>
+    )
+  }
+});
+
+module.exports = Component;
