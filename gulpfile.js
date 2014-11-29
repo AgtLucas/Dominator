aeded820b910691a9e7353b1aab7c7649a109fa3
Lucas <lucas@agtlucas.com>
2014-11-29 01:20:51 -0200

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,10 @@
 
 var gulp = require('gulp'),
   browserify = require('gulp-browserify');
+
+gulp.task('browserify', function () {
+  gulp.src('src/js/main.js')
+    .pipe(browserify({transform: 'reactify'}))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/js'));
+});
