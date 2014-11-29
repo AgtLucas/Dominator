f5319c67defabbc2c2901fdedec08e18e4420912
Lucas <lucas@agtlucas.com>
2014-11-29 11:22:13 -0200

    Update components

--- components/Html.jsx
+++ components/Html.jsx
@@ -9,15 +9,15 @@ var Component = React.createClass({
           <meta charSet="utf-8" />
           <title>{this.props.title}</title>
           <meta name="viewport" content="width=device-width, user-scalable=no" />
-          <link rel="stylesheet" href="/public/main.css">
+          <link rel="stylesheet" href="/public/main.css" />
         </head>
         <body>
           <section id="todoapp" dangerouslySetInnerHTML={{__html: this.props.markup}}></section>
           <script dangerouslySetInnerHTML={{__html: this.props.state}}></script>
-          <script src="/public/js/client.js"></script>
+          <script src="/public/js/client.js" defer></script>
         </body>
       </html>
-    )
+    );
   }
 });
 
